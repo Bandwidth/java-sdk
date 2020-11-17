ea1ec66159ca633cf2b3d0f268b7536ba354c3e6
John Chavez <52748091+jchavez443@users.noreply.github.com>
2020-11-17 13:54:49 -0500

    Java deploy (#10)
    * Java deploy
    
    * Formatted the pom.xml
    
    * rename
    
    * on pull request (#11)
    
    * Update to deploy on releases on main
    
    Co-authored-by: Mike Hamer <mhamer@bandwidth.com>

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --passphrase=$GPG_KEY_PASSPHRASE --output ./.github/secrets/private.key --decrypt ./.github/secrets/private.key.enc
+
+gpg --import ./.github/secrets/private.key
+gpg --import ./.github/secrets/public.key
+
+ls -la
+
+cat << EOF > tempsettings.xml
+<settings>
+  <servers>
+      <server>
+        <id>ossrh</id>
+        <username>$MVN_USERNAME</username>
+        <password>$MVN_PASSWORD</password>
+      </server>
+  </servers>
+</settings>
+EOF
+
+mvn --settings ./tempsettings.xml deploy
