66f6cb34b619a3fb95a5cad4c18d549962c5043d
Mike Hamer <mhamer@bandwidth.com>
2021-08-02 13:14:00 -0400

    Remove previous actions and related files

--- .github/actions/maven-install/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-gpg --quiet --batch --yes --passphrase=$GPG_KEY_PASSPHRASE --output ./.github/secrets/private.key --decrypt ./.github/secrets/private.key.enc
-
-gpg --import ./.github/secrets/private.key
-gpg --import ./.github/secrets/public.key
-
-mvn install
