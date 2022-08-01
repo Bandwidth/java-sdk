dc144fd7731ad0492c6e856cbd57651827a34b94
Brian <bgome004@gmail.com>
2022-08-01 16:11:05 -0400

    DX-2700

--- src/test/java/com/bandwidth/BxmlTest.java
+++ src/test/java/com/bandwidth/BxmlTest.java
@@ -467,7 +467,7 @@ public class BxmlTest {
             .add(startStream)
             .toBXML();
 
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><StartStream destination=\"https://url.com\" streamEventMethod=\"POST\" username=\"user\" password=\"pass\" name=\"test\" streamEventUrl=\"https://url.com\"/></Response>";
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><StartStream destination=\"https://url.com\" name=\"test\" streamEventUrl=\"https://url.com\" streamEventMethod=\"POST\" username=\"user\" password=\"pass\"/></Response>";
 
         assertEquals("BXML strings not equal", expected, response);
     }    
