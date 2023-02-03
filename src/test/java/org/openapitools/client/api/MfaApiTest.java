172836728a41799cf20cd10ce0ddaa965c2f1bd5
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2023-02-03 09:41:01 -0500

    Add MFA Tests

--- src/test/java/org/openapitools/client/api/MfaApiTest.java
+++ src/test/java/org/openapitools/client/api/MfaApiTest.java
@@ -6,10 +6,14 @@ import org.openapitools.client.ApiClient;
 import org.openapitools.client.auth.HttpBasicAuth;
 import org.openapitools.client.model.CodeRequest;
 import org.openapitools.client.model.MessagingCodeResponse;
+import org.openapitools.client.model.VerifyCodeRequest;
+import org.openapitools.client.model.VerifyCodeResponse;
+import org.openapitools.client.model.VoiceCodeResponse;
 import org.openapitools.client.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.net.URI;
 
 import javax.security.auth.AuthPermission;
@@ -47,33 +51,103 @@ public class MfaApiTest {
     }
 
     @Test
-    public void successfulMfaGenerateVoiceCodeRequest() {
+    public void successfulMfaGenerateVoiceCodeRequest() throws ApiException {
         Basic.setUsername(BW_USERNAME);
         Basic.setPassword(BW_PASSWORD);
+
+        CodeRequest request = new CodeRequest();
+        request.setTo(USER_NUMBER);
+        request.setFrom(BW_NUMBER);
+        request.setApplicationId(BW_VOICE_APPLICATION_ID);
+        request.setScope("scope");
+        request.setMessage("Your temporary {NAME} {SCOPE} code is {CODE}");
+        request.setDigits(6);
+        ApiResponse<VoiceCodeResponse> response = api.generateVoiceCodeWithHttpInfo(BW_ACCOUNT_ID, request);
+        assertThat(response.getStatusCode(), is(200));
+
+        assertThat(response.getData(), instanceOf(VoiceCodeResponse.class));
     }
 
     @Test
-    public void successfulMfaVerifyCodeRequest() {
+    public void successfulMfaVerifyCodeRequest() throws ApiException {
         Basic.setUsername(BW_USERNAME);
         Basic.setPassword(BW_PASSWORD);
+        BigDecimal expirationTime = new BigDecimal(3);
+
+        // Generate a random TN for the setTo - otherwise we get heavily rate limited
+        Integer minTn = 1111111111;
+        Long maxTn = 9999999999L;
+        Integer random_int = (int) Math.floor(Math.random() * (maxTn - minTn + 1) + minTn);
+
+        VerifyCodeRequest request = new VerifyCodeRequest();
+        request.setTo("+1" + random_int.toString());
+        request.setScope("2FA");
+        request.setExpirationTimeInMinutes(expirationTime);
+        request.setCode("123456");
+
+        ApiResponse<VerifyCodeResponse> response = api.verifyCodeWithHttpInfo(BW_ACCOUNT_ID, request);
+
+        assertThat(response.getStatusCode(), is(200));
+        assertThat(response.getData().getValid(), is(false));
     }
 
     @Test
-    public void badRequest() {
+    public void badRequest() throws ApiException {
         Basic.setUsername(BW_USERNAME);
         Basic.setPassword(BW_PASSWORD);
+
+        CodeRequest badRequest = new CodeRequest();
+        badRequest.setTo(USER_NUMBER);
+        badRequest.setFrom(BW_NUMBER);
+        badRequest.setApplicationId("not_an_application_id");
+        badRequest.setScope("scope");
+        badRequest.setMessage("Your temporary {NAME} {SCOPE} code is {CODE}");
+        badRequest.setDigits(6);
+
+        ApiException messagingException = Assertions.assertThrows(ApiException.class,
+                () -> api.generateMessagingCodeWithHttpInfo(BW_ACCOUNT_ID,
+                        badRequest));
+        assertThat(messagingException.getCode(), is(400));
+
+        ApiException voiceException = Assertions.assertThrows(ApiException.class,
+                () -> api.generateVoiceCodeWithHttpInfo(BW_ACCOUNT_ID,
+                        badRequest));
+        assertThat(voiceException.getCode(), is(400));
     }
 
     @Test
-    public void unauthorizedRequest() {
-        Basic.setUsername("bad_username");
-        Basic.setPassword("bad_password");
+    public void unauthorizedRequest() throws ApiException {
+        CodeRequest request = new CodeRequest();
+        request.setTo(USER_NUMBER);
+        request.setFrom(BW_NUMBER);
+        request.setApplicationId(BW_VOICE_APPLICATION_ID);
+        request.setScope("scope");
+        request.setMessage("Your temporary {NAME} {SCOPE} code is {CODE}");
+        request.setDigits(6);
+
+        ApiException exception = Assertions.assertThrows(ApiException.class,
+                () -> api.generateMessagingCodeWithHttpInfo(BW_ACCOUNT_ID,
+                        request));
+        assertThat(exception.getCode(), is(401));
     }
 
     @Test
     public void forbiddenRequest() {
         Basic.setUsername(FORBIDDEN_USERNAME);
-        Basic.setPassword(FORBIDDEN_PASSWORD);
+        Basic.setPassword("bad_password");
+
+        CodeRequest request = new CodeRequest();
+        request.setTo(USER_NUMBER);
+        request.setFrom(BW_NUMBER);
+        request.setApplicationId(BW_MESSAGING_APPLICATION_ID);
+        request.setScope("scope");
+        request.setMessage("Your temporary {NAME} {SCOPE} code is {CODE}");
+        request.setDigits(6);
+
+        ApiException exception = Assertions.assertThrows(ApiException.class,
+                () -> api.generateMessagingCodeWithHttpInfo(BW_ACCOUNT_ID,
+                        request));
+        assertThat(exception.getCode(), is(403));
     }
 
 }
