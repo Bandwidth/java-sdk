f91a8cb61b8f4055425c7e018ef5b803a18d3b04
Matthew Martin <105743990+matthewkmartin@users.noreply.github.com>
2023-03-23 08:18:25 -0400

    Task: Update Tests for new Manteca Content Type. (#90)

--- src/test/java/org/openapitools/client/api/ConferencesApiTest.java
+++ src/test/java/org/openapitools/client/api/ConferencesApiTest.java
@@ -127,7 +127,7 @@ public class ConferencesApiTest {
                     .post(mantecaRequestBody)
                     .build();
             Call mantecaApiCall = mantecaClient.newCall(mantecaRequest);
-            testId = mantecaApiCall.execute().body().string().replace("\"", "");
+            testId = mantecaApiCall.execute().body().string();
         } catch (IOException e) {
             System.out.println(e.toString());
             throw new Exception("Failed to initialize conference tests with Manteca, aborting test run :(");
