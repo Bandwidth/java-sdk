014d38e3081f0b844f7749f95486b7a8044381de
DX-Bandwidth <dx@bandwidth.com>
2025-02-12 19:55:35 +0000

    Generate SDK with OpenAPI Generator Version

--- src/main/java/com/bandwidth/sdk/Configuration.java
+++ src/main/java/com/bandwidth/sdk/Configuration.java
@@ -15,7 +15,7 @@ package com.bandwidth.sdk;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.10.0")
 public class Configuration {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.0-dev";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
