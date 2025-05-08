f0b586837cdd77cc2250eb495e80eec1ee9e1385
ckoegel <ckoegel1006@gmail.com>
2025-05-08 10:40:42 -0400

    smoke tests

--- src/test/java/com/bandwidth/sdk/smoke/MultiChannelApiTest.java
+++ src/test/java/com/bandwidth/sdk/smoke/MultiChannelApiTest.java
@@ -1,4 +1,4 @@
-package com.bandwidth.sdk.unit.api;
+package com.bandwidth.sdk.smoke;
 
 import com.bandwidth.sdk.ApiClient;
 import com.bandwidth.sdk.ApiException;
@@ -6,18 +6,24 @@ import com.bandwidth.sdk.ApiResponse;
 import com.bandwidth.sdk.Configuration;
 import com.bandwidth.sdk.api.MultiChannelApi;
 import com.bandwidth.sdk.auth.HttpBasicAuth;
+import com.bandwidth.sdk.model.MmsMessageContent;
 import com.bandwidth.sdk.model.MultiChannelChannelListObject;
 import com.bandwidth.sdk.model.MultiChannelChannelListObjectContent;
 import com.bandwidth.sdk.model.MultiChannelMessageChannelEnum;
 import com.bandwidth.sdk.model.MultiChannelMessageRequest;
 import com.bandwidth.sdk.model.PriorityEnum;
+import com.bandwidth.sdk.model.RbmMessageContentText;
 import com.bandwidth.sdk.model.SmsMessageContent;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.bandwidth.sdk.utils.TestingEnvironmentVariables.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,23 +39,21 @@ public class MultiChannelApiTest {
     public static void setUp() {
         Basic.setUsername(BW_USERNAME);
         Basic.setPassword(BW_PASSWORD);
-        api.setCustomBaseUrl("http://127.0.0.1:4010");
     }
 
     @Test
-    @Disabled // skip because prism can't handle a oneOf with differing required fields
     public void createMultiChannelMessageTest() throws ApiException {
         MultiChannelChannelListObject channelListObject = new MultiChannelChannelListObject()
                 .from(BW_NUMBER)
                 .applicationId(BW_MESSAGING_APPLICATION_ID)
-                .channel(MultiChannelMessageChannelEnum.SMS)
-                .content(new MultiChannelChannelListObjectContent(new SmsMessageContent()
-                        .text("Hello World!")));
+                .channel(MultiChannelMessageChannelEnum.RBM)
+                .content(new MultiChannelChannelListObjectContent(new RbmMessageContentText()
+                                .text("Hello World!")));
         MultiChannelMessageRequest multiChannelMessageRequest = new MultiChannelMessageRequest()
                 .to(USER_NUMBER)
                 .tag("tag")
-                .priority(PriorityEnum.DEFAULT)
-                .expiration(OffsetDateTime.now());
+                .priority(PriorityEnum.HIGH)
+                .expiration(OffsetDateTime.now().plusSeconds(60));
 
         multiChannelMessageRequest.addChannelListItem(channelListObject);
 
