3d968c111c27e0adee83105360d52b104737b909
ckoegel <ckoegel1006@gmail.com>
2025-07-31 17:14:02 -0400

    bring back diversion reason and treatment

---
+++ src/main/java/com/bandwidth/sdk/model/DiversionTreatment.java
@@ -0,0 +1,37 @@
+package com.bandwidth.sdk.model;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public enum DiversionTreatment {
+    none("none"),
+    propagate("propagate"),
+    stack("stack"),
+    ;
+
+    private static final Map<String, DiversionTreatment> DESERIALIZATION_MAP =
+            Arrays.stream(values())
+                    .collect(toMap(v -> v.name, identity()));
+
+    private final String name;
+
+    DiversionTreatment(String name) {
+        this.name = name;
+    }
+
+    @JsonCreator
+    public static DiversionTreatment resolve(String name) {
+        return DESERIALIZATION_MAP.get(name.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
