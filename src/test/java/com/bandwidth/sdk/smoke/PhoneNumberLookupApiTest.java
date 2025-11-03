2f63c19acff1fc929690ae1fcdb29968534874ea
ckoegel <ckoegel1006@gmail.com>
2025-11-03 11:28:40 -0500

    sleep 10 as recommended by numbers team

--- src/test/java/com/bandwidth/sdk/smoke/PhoneNumberLookupApiTest.java
+++ src/test/java/com/bandwidth/sdk/smoke/PhoneNumberLookupApiTest.java
@@ -87,7 +87,7 @@ public class PhoneNumberLookupApiTest {
         assertThat(createResponse.getData().getData().getStatus(), instanceOf(InProgressLookupStatusEnum.class));
         UUID requestId = createResponse.getData().getData().getRequestId();
 
-        TimeUnit.SECONDS.sleep(5);
+        TimeUnit.SECONDS.sleep(10);
 
         ApiResponse<GetAsyncBulkLookupResponse> getResponse
                 = api.getAsyncBulkLookupWithHttpInfo(BW_ACCOUNT_ID, requestId);
