a6989fc6b34b3d48a1001d04cfeae5806e458755
ckoegel <ckoegel1006@gmail.com>
2025-11-05 17:12:44 -0500

    api unit test

--- src/test/java/com/bandwidth/sdk/unit/api/MultiChannelApiTest.java
+++ src/test/java/com/bandwidth/sdk/unit/api/MultiChannelApiTest.java
@@ -6,23 +6,46 @@ import com.bandwidth.sdk.ApiResponse;
 import com.bandwidth.sdk.Configuration;
 import com.bandwidth.sdk.api.MultiChannelApi;
 import com.bandwidth.sdk.auth.HttpBasicAuth;
-// import com.bandwidth.sdk.model.MultiChannelChannelListObject;
-// import com.bandwidth.sdk.model.MultiChannelChannelListObjectContent;
+import com.bandwidth.sdk.model.CreateMultiChannelMessageResponse;
+import com.bandwidth.sdk.model.MessageDirectionEnum;
+import com.bandwidth.sdk.model.MmsMessageContent;
+import com.bandwidth.sdk.model.MmsMessageContentFile;
+import com.bandwidth.sdk.model.MultiChannelAction;
+import com.bandwidth.sdk.model.MultiChannelChannelListMMSObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListMMSResponseObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListRBMObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListRBMObjectAllOfContent;
+import com.bandwidth.sdk.model.MultiChannelChannelListRBMResponseObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListRequestObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListResponseObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListSMSObject;
+import com.bandwidth.sdk.model.MultiChannelChannelListSMSResponseObject;
 import com.bandwidth.sdk.model.MultiChannelMessageChannelEnum;
 import com.bandwidth.sdk.model.MultiChannelMessageRequest;
+import com.bandwidth.sdk.model.MultiChannelMessageResponseData;
 import com.bandwidth.sdk.model.PriorityEnum;
+import com.bandwidth.sdk.model.RbmActionDial;
+import com.bandwidth.sdk.model.RbmActionTypeEnum;
+import com.bandwidth.sdk.model.RbmMessageContentText;
 import com.bandwidth.sdk.model.SmsMessageContent;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 import static com.bandwidth.sdk.utils.TestingEnvironmentVariables.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 
+@SuppressWarnings("null")
 public class MultiChannelApiTest {
     private static ApiClient defaultClient = Configuration.getDefaultApiClient();
     private static HttpBasicAuth Basic = (HttpBasicAuth) defaultClient.getAuthentication("Basic");
@@ -35,26 +58,38 @@ public class MultiChannelApiTest {
         api.setCustomBaseUrl("http://127.0.0.1:4010");
     }
 
-    // @Test
-    // @Disabled // skip because prism can't handle a oneOf with differing required fields
-    // public void createMultiChannelMessageTest() throws ApiException {
-    //     MultiChannelChannelListObject channelListObject = new MultiChannelChannelListObject()
-    //             .from(BW_NUMBER)
-    //             .applicationId(BW_MESSAGING_APPLICATION_ID)
-    //             .channel(MultiChannelMessageChannelEnum.SMS)
-    //             .content(new MultiChannelChannelListObjectContent(new SmsMessageContent()
-    //                     .text("Hello World!")));
-    //     MultiChannelMessageRequest multiChannelMessageRequest = new MultiChannelMessageRequest()
-    //             .to(USER_NUMBER)
-    //             .tag("tag")
-    //             .priority(PriorityEnum.DEFAULT)
-    //             .expiration(OffsetDateTime.now());
+    @Test
+    public void createMultiChannelMessageTest() throws ApiException {
+        MultiChannelChannelListRequestObject channelListSMSObject = new MultiChannelChannelListRequestObject(
+                new MultiChannelChannelListSMSObject()
+                        .from(BW_NUMBER)
+                        .applicationId(BW_MESSAGING_APPLICATION_ID)
+                        .channel(MultiChannelMessageChannelEnum.SMS)
+                        .content(new SmsMessageContent().text("Hello World!"))
+        );
+        MultiChannelMessageRequest smsRequest = new MultiChannelMessageRequest()
+                .to(USER_NUMBER)
+                .tag("tag")
+                .priority(PriorityEnum.HIGH)
+                .expiration(OffsetDateTime.now().plusSeconds(60))
+                .channelList(Arrays.asList(channelListSMSObject));
 
-    //     multiChannelMessageRequest.addChannelListItem(channelListObject);
+        ApiResponse<CreateMultiChannelMessageResponse> response
+                = api.createMultiChannelMessageWithHttpInfo(BW_ACCOUNT_ID, smsRequest);
 
-    //     ApiResponse response =
-    //         api.createMultiChannelMessageWithHttpInfo(BW_ACCOUNT_ID, multiChannelMessageRequest);
-    //     assertThat(response.getStatusCode(), is(202));
-    // }
+        assertThat(response.getStatusCode(), is(202));
+        assertThat(response.getData(), instanceOf(CreateMultiChannelMessageResponse.class));
+        assertThat(response.getData().getLinks(), instanceOf(List.class));
+        assertThat(response.getData().getData(), instanceOf(MultiChannelMessageResponseData.class));
+        assertThat(response.getData().getData().getId(), instanceOf(String.class));
+        assertThat(response.getData().getData().getTime(), instanceOf(OffsetDateTime.class));
+        assertThat(response.getData().getData().getDirection(), instanceOf(MessageDirectionEnum.class));
+        assertThat(response.getData().getData().getTo(), instanceOf(Set.class));
+        assertThat(response.getData().getData().getTag(), instanceOf(String.class));
+        assertThat(response.getData().getData().getPriority(), instanceOf(PriorityEnum.class));
+        assertThat(response.getData().getData().getExpiration(), instanceOf(OffsetDateTime.class));
+        assertThat(response.getData().getData().getChannelList(), instanceOf(List.class));
+        assertThat(response.getData().getData().getChannelList().get(0), instanceOf(MultiChannelChannelListResponseObject.class));
+    }
 
 }
