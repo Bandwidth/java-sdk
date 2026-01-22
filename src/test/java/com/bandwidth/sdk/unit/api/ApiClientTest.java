125e5e76f7a0966801fc3f203884cbc0a95e3705
ckoegel <ckoegel1006@gmail.com>
2026-01-22 11:03:01 -0500

    use hasKey

--- src/test/java/com/bandwidth/sdk/unit/api/ApiClientTest.java
+++ src/test/java/com/bandwidth/sdk/unit/api/ApiClientTest.java
@@ -15,7 +15,7 @@ import static com.bandwidth.sdk.utils.TestingEnvironmentVariables.*;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.hamcrest.Matchers.not;
@@ -45,11 +45,11 @@ public class ApiClientTest {
         Map<String, Authentication> oauthAuthentications = oauthClient.getAuthentications();
         
         assertThat(authentications, notNullValue());
-        assertThat(authentications.containsKey("Basic"), is(true));
+        assertThat(authentications, hasKey("Basic"));
         assertThat(basicClient.getAuthentication("Basic"), instanceOf(HttpBasicAuth.class));
 
         assertThat(oauthAuthentications, notNullValue());
-        assertThat(oauthAuthentications.containsKey("OAuth2"), is(true));
+        assertThat(oauthAuthentications, hasKey("OAuth2"));
         assertThat(oauthClient.getAuthentication("OAuth2"), instanceOf(RetryingOAuth.class));
     }
 
