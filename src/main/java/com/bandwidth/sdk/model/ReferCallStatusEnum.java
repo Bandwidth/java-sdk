0c110eb60d3c13488b52fac2dff733cadf5535b3
atelegu <atelegu@bandwidthcontractors.com>
2026-06-24 20:15:00 +0530

    VAPI-3164

--- src/main/java/com/bandwidth/sdk/model/ReferCallStatusEnum.java
+++ src/main/java/com/bandwidth/sdk/model/ReferCallStatusEnum.java
@@ -1,9 +1,20 @@
 package com.bandwidth.sdk.model;
 
+import java.io.IOException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.JsonElement;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+/**
+ * The outcome of the SIP REFER attempt.
+ */
+@JsonAdapter(ReferCallStatusEnum.Adapter.class)
 public enum ReferCallStatusEnum {
-    
+
     @SerializedName("success")
     SUCCESS("success"),
 
@@ -34,7 +45,20 @@ public enum ReferCallStatusEnum {
         throw new IllegalArgumentException("Unexpected value '" + value + "'");
     }
 
-    public static void validateJsonElement(com.google.gson.JsonElement jsonElement) throws java.io.IOException {
+    public static class Adapter extends TypeAdapter<ReferCallStatusEnum> {
+        @Override
+        public void write(final JsonWriter jsonWriter, final ReferCallStatusEnum enumeration) throws IOException {
+            jsonWriter.value(enumeration.getValue());
+        }
+
+        @Override
+        public ReferCallStatusEnum read(final JsonReader jsonReader) throws IOException {
+            String value = jsonReader.nextString();
+            return ReferCallStatusEnum.fromValue(value);
+        }
+    }
+
+    public static void validateJsonElement(JsonElement jsonElement) throws IOException {
         String value = jsonElement.getAsString();
         ReferCallStatusEnum.fromValue(value);
     }
